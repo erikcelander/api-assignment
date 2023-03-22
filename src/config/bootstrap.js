bc3926706da5440348337025cc564806d5b0ab5e
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-03-22 17:54:35 +0100

    set up database

--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -6,35 +6,35 @@
  */
 
 import { IoCContainer } from '../util/IoCContainer.js'
-import { TaskModel } from '../models/TaskModel.js'
-import { TaskRepository } from '../repositories/TaskRepository.js'
-import { TasksService } from '../services/TasksService.js'
-import { TasksController } from '../controllers/TasksController.js'
+// import { TaskModel } from '../models/TaskModel.js'
+// import { TaskRepository } from '../repositories/TaskRepository.js'
+// import { TasksService } from '../services/TasksService.js'
+// import { TasksController } from '../controllers/TasksController.js'
 
 const iocContainer = new IoCContainer()
 
 iocContainer.register('ConnectionString', process.env.DB_CONNECTION_STRING)
 
-iocContainer.register('TaskModelType', TaskModel, { type: true })
+// iocContainer.register('TaskModelType', TaskModel, { type: true })
 
-iocContainer.register('TaskRepositorySingleton', TaskRepository, {
-  dependencies: [
-    'TaskModelType'
-  ],
-  singleton: true
-})
+// iocContainer.register('TaskRepositorySingleton', TaskRepository, {
+//   dependencies: [
+//     'TaskModelType'
+//   ],
+//   singleton: true
+// })
 
-iocContainer.register('TasksServiceSingleton', TasksService, {
-  dependencies: [
-    'TaskRepositorySingleton'
-  ],
-  singleton: true
-})
+// iocContainer.register('TasksServiceSingleton', TasksService, {
+//   dependencies: [
+//     'TaskRepositorySingleton'
+//   ],
+//   singleton: true
+// })
 
-iocContainer.register('TasksController', TasksController, {
-  dependencies: [
-    'TasksServiceSingleton'
-  ]
-})
+// iocContainer.register('TasksController', TasksController, {
+//   dependencies: [
+//     'TasksServiceSingleton'
+//   ]
+// })
 
 export const container = Object.freeze(iocContainer)
