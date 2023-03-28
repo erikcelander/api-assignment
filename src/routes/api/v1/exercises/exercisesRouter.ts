ccf46c0cbd40410a213e56318fc55f29ffed9ae1
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-03-28 22:56:31 +0200

    add exercise routes

--- src/routes/api/v1/exercises/exercisesRouter.ts
+++ src/routes/api/v1/exercises/exercisesRouter.ts
@@ -10,8 +10,19 @@ const controller = container.resolve('ExercisesController') as ExerciseControlle
 
 router.param('id', (req: Request, res: Response, next: NextFunction, id: string) => controller.loadExercise(req, res, next, id))
 
+// GET /api/v1/exercises
 router.get('/', (req: Request, res: Response, next: NextFunction) => controller.get(req, res, next))
-
 router.get('/:id', (req: Request, res: Response, next: NextFunction) => controller.getAll(req, res, next))
 
+// POST /api/v1/exercises
 router.post('/add', (req: Request, res: Response, next: NextFunction) => controller.add(req, res, next))
+
+// PATCH /api/v1/exercises/:id
+router.patch('/:id', (req: Request, res: Response, next: NextFunction) => controller.partiallyUpdate(req, res, next))
+
+// PUT /api/v1/exercises/:id
+router.put('/:id', (req: Request, res: Response, next: NextFunction) => controller.update(req, res, next))
+
+// DELETE /api/v1/exercises/:id
+router.delete('/:id', (req: Request, res: Response, next: NextFunction) => controller.delete(req, res, next))
+
